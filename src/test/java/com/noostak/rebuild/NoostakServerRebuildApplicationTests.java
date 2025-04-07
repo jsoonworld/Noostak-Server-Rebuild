3291f6336318926ad8a7622ded2a1b730c5c1ccc
권장순 <jsoonworld@gmail.com>
2025-04-07 13:59:50 +0900

    test(application) : 테스트 환경에서 H2 DB를 사용하도록 test 프로파일 적용

--- src/test/java/com/noostak/rebuild/NoostakServerRebuildApplicationTests.java
+++ src/test/java/com/noostak/rebuild/NoostakServerRebuildApplicationTests.java
@@ -2,12 +2,13 @@ package com.noostak.rebuild;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NoostakServerRebuildApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
+
