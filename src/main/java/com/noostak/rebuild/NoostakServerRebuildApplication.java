18c2a68d16321d39f6428fa9643f1551200896b8
권장순 <jsoonworld@gmail.com>
2025-04-07 11:35:19 +0900

    feat(Server): initial project setup

---
+++ src/main/java/com/noostak/rebuild/NoostakServerRebuildApplication.java
@@ -0,0 +1,13 @@
+package com.noostak.rebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoostakServerRebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoostakServerRebuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noostak/rebuild/NoostakServerRebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noostak.rebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoostakServerRebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
