4478b04611230d5c33007fa1ccce40fc38b87033
권장순 <jsoonworld@gmail.com>
2025-04-07 18:45:10 +0900

    feat(MemberProfileImageKey) : 🟢 프로파일 이미지 키가 널인 경우를 검증하는 로직 구현

--- src/main/java/com/noostak/rebuild/member/vo/MemberProfileImageKey.java
+++ src/main/java/com/noostak/rebuild/member/vo/MemberProfileImageKey.java
@@ -1,4 +1,38 @@
 package com.noostak.rebuild.member.vo;
 
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+
+@Embeddable
+@EqualsAndHashCode
 public class MemberProfileImageKey {
+
+    private final String value;
+
+    protected MemberProfileImageKey() {
+        this.value = null;
+    }
+
+    private MemberProfileImageKey(String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    public static MemberProfileImageKey from(String value) {
+        return new MemberProfileImageKey(value);
+    }
+
+    public String value() {
+        return value;
+    }
+
+    private void validate(String value) {
+        validateNotNull(value);
+    }
+
+    private void validateNotNull(String value) {
+        if (value == null) {
+            throw new IllegalArgumentException("프로필 이미지 키는 null 일 수 없습니다.");
+        }
+    }
 }
